3539363ad533861685046d404ad9d89415c68690
Ana Villanueva <54164289+anagvillanueva@users.noreply.github.com>
2026-02-03 20:34:19 -0600

    Ejemplo de lista del super

--- ciclos.js
+++ ciclos.js
@@ -45,3 +45,41 @@ console.log(alumnos);
 alumnos.shift();
 console.log(alumnos);
 
+// Arreglo de una lista de super 
+// Impriman su lista del super 
+
+// 1. Manzana 
+// 2. Papas 
+// 3. Papel de bano
+// 4. Jabon
+
+
+let listaDeSuper = ['Manzanas', 'Servilletas', 'Jabon', 'Aguacate', 'Tortillas']
+
+for (let i = 0; i < listaDeSuper.length; i++ ){
+    console.log((i+1) + "." + listaDeSuper[i])
+}
+console.log("Total de la lista=", listaDeSuper.length)
+
+
+// EXTRA 1 Total de productos = ____
+
+// EXTRA 2 Propmt (Guardar la lista que te da el usuario, dentro de un arreglo)
+
+
+let listaSuper = [];
+let producto = "inicio";
+
+while (producto !== ""){
+    producto = prompt('Ingresa un producto del super.')
+
+    if (producto !== ""){
+        listaSuper.push(producto);
+    }
+}
+
+for (let indice = 0; indice < listaSuper.length; indice++){
+    console.log((indice+1) + " - " + listaSuper[indice]);
+}
+
+console.log("Total de la lista=", listaSuper.length)
