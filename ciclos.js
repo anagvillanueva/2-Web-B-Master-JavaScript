53cd4593299b99603988c2acdc23471012838acc
Ana Villanueva <54164289+anagvillanueva@users.noreply.github.com>
2026-02-03 20:00:23 -0600

    Arreglos

--- ciclos.js
+++ ciclos.js
@@ -10,7 +10,6 @@ while (i <= 10 ){
 console.log('--------------------');
 
 //Arrays 
-
 let frutas = ['Manzana', 'Pera', 'Lulo', 'Cereza', 'Mango', 'Papaya'];
 
 console.log(frutas[2])
@@ -22,3 +21,27 @@ console.log(frutas.length)
 for (let indice = 0; indice < frutas.length; indice++){
     console.log("Fruta = ", frutas[indice])
 }
+
+console.log('Metodos de los arreglos')
+let alumnos = []; // declarando un arreglo vacio
+
+// Metodo push, agrega al final
+alumnos.push("Ana");
+alumnos.push("Bruno");
+alumnos.push("Fernanda");
+alumnos.push("Natalia");
+console.log(alumnos);
+// Metodo pop quita el ultimo
+alumnos.pop();
+console.log(alumnos);
+
+//Metodo unshift agrega al principio
+alumnos.unshift("Nina");
+alumnos.unshift("Misael");
+alumnos.unshift("Beto");
+
+console.log(alumnos);
+// Metodo shift quita al principio 
+alumnos.shift();
+console.log(alumnos);
+
