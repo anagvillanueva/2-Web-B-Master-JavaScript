48272d03d22298091b1fd182411df75b135f964f
Ana Villanueva <54164289+anagvillanueva@users.noreply.github.com>
2026-02-19 20:58:39 -0600

    Clase de localStorage

--- main.js
+++ main.js
@@ -1,33 +1,101 @@
-//Paso 1) Seleccionar mis elementos que voy a ocupar
+/*
+localStorage: Nos va permitir guardar datos del usuario
+NO se van a borrar al cerrar la pestana o el navegador 
+Funciona por dominio
 
-let itemInput = document.getElementById("itemInput");
-let addItemButton = document.getElementById('addItemButton');
-let itemList = document.getElementById("itemList");
+## SOLO guarda STRINGS (texto) ##
+Si queremos guardar objetos/arrays debemos convertir a JSON 
+localStorage.setItem("usuario", JSON.stringify(objeto))
 
-//Paso 2) Crear mi funcion 
-function addItemButtonClik(){
-    let itemText = itemInput.value;
-    
-    let newItem = document.createElement('li');
+API Web Storage de JavaScript
 
-    //Crear un elemento span 
-    let textSpan = document.createElement('span');
-    textSpan.textContent = itemText;
-    newItem.appendChild(textSpan)
 
-    //Crear el boton eliminar 
-    let deleteButton = document.createElement('button');
-    deleteButton.textContent = 'Eliminar'
+clave --> valor 
 
-    
-    deleteButton.addEventListener('click', function(){
-        newItem.remove();
-    })
+nombre --> Ana 
+idioma --> espanol
 
-    newItem.appendChild(deleteButton)
-    itemList.appendChild(newItem)
-    itemInput.value = '';
+GUARDAR DATOS
+localStorage.setItem("clave", "valor");
+
+OBTENER DATOS 
+localStorage.getItem("clave");
+Si no existe va devolver NULL
+
+ELIMINAR DATOS 
+localStorage.removeItem("clave");
+
+BORRAR TODO EL ALMACENAMIENTO 
+localStorage.clear();
+
+
+Cuando NO debenmos de usarlo:
+- Datos sensibles como contrasenas 
+- Informacion muy grande 
+- Seguridad critica
+
+localStorage
+sessionStorage 
+cookies
+*/
+
+
+// Seleccionar nuestros elementos del DOM 
+
+let txt = document.getElementById('texto');
+let fechaP = document.getElementById('fecha');
+let btnGuardar = document.getElementById('guardar');
+let btnBorrar = document.getElementById('borrar');
+
+// Declarar las claves donde se van a guardar en localStorage 
+
+const CLAVE_TEXTO = 'diario_texto';
+const CLAVE_FECHA = 'diario_fecha'
+
+// Funcion para obtener la fecha del dia de hoy 
+
+function fechaSoloDia(){
+    let hoy = new Date();  // Creamos un objeto con la fecha actual 
+    return hoy.toLocaleDateString('es-MX') 
 }
 
-// Agregamos el evento escuchador click y le asignamos nuestra funcion que creamos 
-addItemButton.addEventListener('click', addItemButtonClik);
+function cargar(){
+    let textoGuardado = localStorage.getItem(CLAVE_TEXTO)
+    let fechaGuardada = localStorage.getItem(CLAVE_FECHA)
+
+    if(textoGuardado){
+        txt.value = textoGuardado;
+    }
+
+    if(fechaGuardada){
+        fechaP.textContent = 'Guardado el:' + fechaGuardada;
+    }else {
+        fechaP.textContent = '';
+    }
+}
+
+// Para darle click en Guardar 
+btnGuardar.onclick = () => {
+    let texto = txt.value.trim(); // Para quitar espacios 
+
+    if(!texto) return; 
+
+    let hoy = fechaSoloDia(); // Funcion que creamos para obtener el dia
+
+    // Guardado como string 
+    localStorage.setItem(CLAVE_TEXTO, texto)
+    localStorage.setItem(CLAVE_FECHA, hoy)
+
+    fechaP.textContent = 'Guardado el: ' + hoy;
+};
+
+// Para darle click en Eliminar 
+btnBorrar.onclick = () => {
+    localStorage.removeItem(CLAVE_TEXTO)
+    localStorage.removeItem(CLAVE_FECHA)
+
+    txt.value = '';
+    fechaP.textContent = '';
+};
+
+cargar(); // Llamar a la funcion cargar al inicio para mostrar lo que ya estaba guardado
